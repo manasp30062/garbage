189a633cfb5449ccd9393416b0e64bce4bd5a88b
manasp30062 <manasp30062@gmail.com>
2026-07-17 23:55:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-model = load_model("waste_classifier.keras")
+interpreter = tf.lite.Interpreter(model_path="waste_classifier.tflite")
+interpreter.allocate_tensors()
 
-class_names = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+class_names = [
+    "cardboard",
+    "glass",
+    "metal",
+    "paper",
+    "plastic",
+    "trash"
+]
+
+st.set_page_config(page_title="Garbage Classification", page_icon="♻️")
 
 st.title("♻️ Garbage Classification")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image", use_container_width=True)
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    img = img.resize((128, 128))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img = image.resize((128, 128))
+    img = np.array(img, dtype=np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    prediction = model.predict(img_array)
+    interpreter.set_tensor(input_details[0]["index"], img)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]["index"])[0]
 
     predicted_class = class_names[np.argmax(prediction)]
-    confidence = np.max(prediction) * 100
+    confidence = float(np.max(prediction) * 100)
 
     st.success(f"Prediction: {predicted_class}")
     st.write(f"Confidence: {confidence:.2f}%")
